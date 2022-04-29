71362ae03efce7850131f2910d8c73f921c75cb6
NewChakrit <imnew108@gmail.com>
2022-04-29 08:32:07 -0700

    'save'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Photos from "./components/Photos";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+
+  const apiKey = "wHGvrk7tWmqHbJCscFqMnDBHcXqq2D0DVFf5nAq8nGA";
+  const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&page=1`;
+
+  const fetchImg = async () => {
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    console.log(data);
+    setPhotos(data);
+  };
+
+  useEffect(() => {
+    fetchImg();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Photos />
     </div>
   );
 }
